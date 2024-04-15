0f7a09077bafe90d129cbe272f666e96f08cc1d8
Diego C. Silva <diegocaetano444@outlook.com>
2024-04-15 18:04:20 -0300

    refactor: Updating ManageUser to according the single responsibility principle

--- src/main/java/me/dcdev/gof/desafios/gerenciar_usuario/GerenciarUsuarioMain.java
+++ src/main/java/me/dcdev/gof/desafios/gerenciar_usuario/GerenciarUsuarioMain.java
@@ -25,28 +25,27 @@ class User {
 
 class UserManager {
     private static UserManager instancia;
-    private static List<User> users;
-
-    private UserManager(List<User> users) {
-        this.users = users;
+    
+    private UserManager() {
+        super();
     }
 
-    public static UserManager getInstancia(List<User> users) {
+    public static UserManager getInstancia() {
         if(instancia == null) {
-            instancia = new UserManager(users);
+            instancia = new UserManager();
         }
 
         return instancia;
     }
 
-    public void registerUser(String userName) {
+    public void registerUser(List<User> users, String userName) {
         int id = users.size() +1;
         User new_user = new User(id, userName);
 
-        users.add(new_user); // lista de usuário estatica
+        users.add(new_user);
     }
 
-    public void listUsers() {
+    public void listUsers(List<User> users) {
         users.forEach(u -> {
             System.out.println(String.format("%d - %s", u.getId(), u.getName()));
         });
@@ -56,21 +55,21 @@ class UserManager {
 public class GerenciarUsuarioMain {
 
     private static final List<User> users = new ArrayList<>();
-    private static UserManager userManager = UserManager.getInstancia(users);
+    private static UserManager userManager = UserManager.getInstancia();
 
     public static void addUser(String name) {
-        userManager.registerUser(name);
+        userManager.registerUser(users, name);
     }
 
     public static void listUsers() {
-        userManager.listUsers();
+        userManager.listUsers(users);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int quantity = scanner.nextInt();
-        scanner.nextLine();  // Consumir a quebra de linha após a leitura do número
+        scanner.nextLine();
 
         for (int i = 1; i <= quantity; i++) {
             String name = scanner.nextLine();
